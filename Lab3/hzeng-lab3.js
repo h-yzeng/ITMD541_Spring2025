c450f455747068dcd1081e72a8e305de349bee93
hzeng9 <hzeng9@hawk.iit.edu>
2025-03-02 15:48:18 -0600

    Finished Exercise 1 and Test Cases works as expected

--- Lab3/hzeng-lab3.js
+++ Lab3/hzeng-lab3.js
@@ -1,9 +1,19 @@
 // H Zeng
 // ITMD 541-02 Graduate Student
 
-// Excercise 1
+// Exercise 1
+function minMaxAverage(numbers) {
+    const totalNumbers = numbers.length;
+    const minValue = Math.min(...numbers);
+    const maxValue = Math.max(...numbers);
+    const average = numbers.reduce((sum, num) => sum + num) / totalNumbers;
+    console.log(`Total Numbers: ${totalNumbers}, Min Value: ${minValue}, Max Value: ${maxValue}, Average: ${average.toFixed(2)}`);
+}
 
-
+// Test Cases - Exercise 1
+minMaxAverage([2, 5, 23, 6, 9, 4, 30, 1]);
+minMaxAverage([1, 5, 3, 5, 10, 12, 8, 6]);
+minMaxAverage([2, 4, 6, 8, 10, 12, 14, 16]);
 
 // Exercise 2
 
