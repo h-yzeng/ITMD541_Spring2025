e28f38d6086c9148e0d948e092d7fa66e49ceb45
hzeng9 <hzeng9@hawk.iit.edu>
2025-03-02 16:06:21 -0600

    Completed Exercise 2 and Exercise 3 and test cases works for both exercises

--- Lab3/hzeng-lab3.js
+++ Lab3/hzeng-lab3.js
@@ -16,11 +16,28 @@ minMaxAverage([1, 5, 3, 5, 10, 12, 8, 6]);
 minMaxAverage([2, 4, 6, 8, 10, 12, 14, 16]);
 
 // Exercise 2
+function countVowels(word) {
+    const vowels = 'aeiouAEIOU';
+    const count = [...word].filter(char => vowels.includes(char)).length;
+    const vowelCount = count === 1 ? 'vowel' : 'vowels';
+    console.log(`${word}: ${count} ${vowelCount}`);
+}
 
-
+// Test Cases - Exercise 2
+countVowels("Winter");
+countVowels("Spring");
+countVowels("Summer");
+countVowels("Autumn");
 
 // Exercise 3
+function sortNumbers(numbers) {
+    return [...numbers].sort((a, b) => a - b);
+}
 
+// Test cases for Exercise 3
+console.log("Original Array:", [9, 4, 6, 2], ", Sorted Array:", sortNumbers([9, 4, 6, 2]));
+console.log("Original Array:", [2, 3, 5, 1], ", Sorted Array:", sortNumbers([2, 3, 5, 1]));
+console.log("Original Array:", [8, 5, 7, 2], ", Sorted Array:", sortNumbers([8, 5, 7, 2]));
 
 
 // Exercise 4
