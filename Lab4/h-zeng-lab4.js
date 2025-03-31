67693e5c1999fe329b57dc6fbf7257f1ed470bf5
hzeng9 <hzeng9@hawk.iit.edu>
2025-03-30 19:34:41 -0500

    Created Lab 4 folder and the initial lab4.js file

---
+++ Lab4/h-zeng-lab4.js
@@ -0,0 +1,25 @@
+// H Zeng
+// ITMD 541-02 Graduate Student
+
+//1. In the hero section change the main headline
+
+//2. Change the line of text below the hero headline 
+
+//3. Change the image in the background of the hero
+
+//4. Change the background color of the nav bar to the same color that is used in the footer
+
+//5. Remove the “Get Started” CTA from the hero.
+
+//6. Center Align the heading for sections – a. Services, b. Solutions, and c. Contact
+
+//7. In the services section change the icons color to (#47C714)
+
+//8. Change the digital marketing icon to use ‘Ads Click’ instead of the current icon.
+
+//9. In the specialized marketing solutions section make a change to the layout of the tiles
+
+//10. In the same section change the Musicians image 
+
+// Graduate Additional Requirements
+
